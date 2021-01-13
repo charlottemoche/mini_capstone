72b50b49dca073a6425202aa2536d33cbd56683f
charlotte <cmoche123@gmail.com>
2021-01-13 15:15:55 -0500

    adds product methods

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,12 @@
 class Api::ProductsController < ApplicationController
+  
+  def product_action
+    @product = Product.first
+    render "product_action.json.jb"
+  end
+
+  def all_products_action
+    @products = Product.all
+    render "all_products.json.jb"
+  end
 end
