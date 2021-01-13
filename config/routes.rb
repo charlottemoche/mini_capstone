e0198b88dbe7764a5a42714551b5422cf3712cfe
charlotte <cmoche123@gmail.com>
2021-01-13 15:27:29 -0500

    adds products with seed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   
   namespace :api do
     get "/single_product_path" => "products#product_action"
+    get "all_products_path" => "products#all_products"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = Product.create([
+  {name: "cheese", price: 7.0, image_url: "https://www.wisconsinrivermeats.com/prodimages/cheddar-cheese.jpg", description: "aged cheddar"},
+  {name: "grapes", price: 2.50, image_url: "https://media.npr.org/assets/img/2013/03/06/f-88c8aa015f1e0c3da52479fae49a77adefdba3a2-s800-c85.jpg", description: "green"},
+  {name: "crackers", price: 4.75, image_url: "https://static.toiimg.com/photo/69114907.cms", description: "sea salt"}
+])
