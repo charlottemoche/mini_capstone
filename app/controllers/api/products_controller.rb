cc7a57efd4989beddcee3866bdda1c482dacb679
charlotte <cmoche123@gmail.com>
2021-01-14 23:17:29 -0500

    removes comma

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,13 +5,14 @@ class Api::ProductsController < ApplicationController
     render "first_product.json.jb"
   end
 
-  def product_action
-    @product = Product.id()
-    render "product_action.json.jb"
+  def second_product_action
+    @product = Product.second
+    render "first_product.json.jb"
   end
 
   def all_products_action
     @products = Product.all
     render "all_products.json.jb"
   end
+  
 end
