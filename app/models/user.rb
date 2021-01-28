3c06b40246a2e98e1c6a4fd0c977e9039d2c2f04
charlotte <cmoche123@gmail.com>
2021-01-27 20:23:17 -0500

    adds today's changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-
-  has_many: orders
   
+  has_many :orders
+
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,11 @@ Rails.application.routes.draw do
 
     post "/users" => "users#create"
     post "/sessions" => "sessions#create"
+
+    get "/orders" => "orders#index"
+    get "/orders/:id" => "orders#show"
+
+    post "/orders" => "orders#create"
+    
   end
 end
